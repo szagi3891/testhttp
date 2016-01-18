42d2a2a3250f4954f69d55b80429163d4bf7f76e
Michal Pokrywka <wolfmoon@o2.pl>
2016-01-18 12:48:26 +0100

    Replaced chan with comm
    Removed chan dependency.
    Used comm::mpmc::bounded:Channel everywhere - could be bad idea.

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -1,12 +1,14 @@
 mod api;
 mod worker;
 
-use chan::{self};
-use asynchttp::{miohttp,log};
-use asynchttp::miohttp::request;
-use std::{process};
+use std::process;
 use simple_signal::{Signals, Signal};
+use comm::mpmc::bounded::Channel;
+use comm::select::{Select, Selectable};
+
+use asynchttp::{miohttp,log};
 use asynchttp::async::{spawn};
+use asynchttp::miohttp::request;
 
 pub fn run_main() {
         
@@ -28,14 +30,14 @@ pub fn run_main() {
 fn run(addres: String) -> i32 {
     
     
-    let (tx_request, rx_request) = chan::async();
+    let request = Channel::new(100);
     
     
     let thread_name = "<EventLoop>".to_owned();
         
-    match spawn(thread_name, move ||{
+    let tx_request = request.clone();
         
-        let tx_request = tx_request.clone();
+    match spawn(thread_name, move ||{
         
         miohttp::server::MyHandler::new(&addres, 4000, 4000, tx_request);
         
@@ -64,19 +66,22 @@ fn run(addres: String) -> i32 {
     
     // Return real OS error to shell, return err.raw_os_error().unwrap_or(-1)
     
-    let (tx_api_request , rx_api_request) = chan::async();         //<(String, api::CallbackFD)>
-    let (tx_api_response, rx_api_response) = chan::async();         //<(api::FilesData, api::CallbackFD)>
+    let api_request  = Channel::new(100);        //<(String, api::CallbackFD)>
+    let api_response = Channel::new(100);        //<(api::FilesData, api::CallbackFD)>
     
-    
-    let thread_name = "<api>".to_owned();
-    
-    match spawn(thread_name, move ||{
-        api::run(rx_api_request, tx_api_response);
-    }) {
-        Ok(join_handle) => join_handle,
-        Err(err) => panic!("Can't spawn StaticHttp spawner: {}", err),
-    };
-    
+    {
+        let thread_name = "<api>".to_owned();
+       
+        let rx_api_request = api_request.clone();
+        let tx_api_response = api_response.clone();
+
+        match spawn(thread_name, move ||{
+            api::run(rx_api_request, tx_api_response);
+        }) {
+            Ok(join_handle) => join_handle,
+            Err(err) => panic!("Can't spawn StaticHttp spawner: {}", err),
+        };
+    }
     
     
                                 //np. 4 workery
@@ -85,9 +90,9 @@ fn run(addres: String) -> i32 {
         
         let thread_name = "<worker>".to_owned();
         
-        let rx_request      = rx_request.clone();
-        let tx_api_request  = tx_api_request.clone();
-        let rx_api_response = rx_api_response.clone();
+        let rx_request      = request.clone();
+        let tx_api_request  = api_request.clone();
+        let rx_api_response = api_response.clone();
 
         match spawn(thread_name, move ||{
             run_worker(rx_request, tx_api_request, rx_api_response);
@@ -99,71 +104,76 @@ fn run(addres: String) -> i32 {
     
     
     
-    let (ctrl_c_tx1, ctrl_c_rx1) = chan::sync(0);
-    let (ctrl_c_tx2, ctrl_c_rx2) = chan::sync(0);
+    let ctrl_c1 = Channel::new(1);
+    let ctrl_c2 = Channel::new(1);
 
-    Signals::set_handler(&[Signal::Int, Signal::Term], move |_signals| {
+    {
+        let ctrl_c1 = ctrl_c1.clone();
+        let ctrl_c2 = ctrl_c2.clone();
+        Signals::set_handler(&[Signal::Int, Signal::Term], move |_signals| {
 
-        log::debug(format!("Termination signal catched."));
+            log::debug(format!("Termination signal catched."));
 
-        ctrl_c_tx1.send(());
-                                        //oczekuj na zakończenie procedury wyłączania
-        let _ = ctrl_c_rx2.recv();
-    });
+            ctrl_c1.send_async(());
 
-
+            // oczekuj na zakończenie procedury wyłączania
+            let _ = ctrl_c2.recv_sync();
+        });
+    }
     
-                //główna pętla sterująca podwątkami
+    // główna pętla sterująca podwątkami
     loop {
         
-        let _ = ctrl_c_rx1.recv();
+        let _ = ctrl_c1.recv_sync();
 
         log::info(format!("Shutting down!"));
-        ctrl_c_tx2.send(());
+        ctrl_c2.send_sync(());
         return 0;
     }
 }
 
 
 
-fn run_worker(rx_request: chan::Receiver<request::Request>, tx_api_request: chan::Sender<api::Request>, rx_api_response: chan::Receiver<api::Response>) {
+fn run_worker<'a>(rx_request: Channel<'a, request::Request>, tx_api_request: Channel<'a, api::Request>, rx_api_response: Channel<'a, api::Response>) {
     
+    let select = Select::new();
+    select.add(&rx_request);
+    select.add(&rx_api_response);
+
     loop {
+        for &mut id in select.wait(&mut [0, 0]) {
+            if id == rx_request.id() {
 
-        chan_select! {
+                match rx_request.recv_sync() {
 
-            rx_request.recv() -> to_handle => {
-
-                match to_handle {
-
-                    Some(request) => {
+                    Ok(request) => {
                         
                         worker::render_request(request, &tx_api_request);
                     }
 
-                    None => {
+                    Err(err) => {
 
                         //TODO
-                        //println!("wyparował nadawca requestów");
+                        println!("ex_request channel error: {:?}", err);
                         return;
                     }
                 }
-            },
+            }
 
-            rx_api_response.recv() -> data => {
+            else if id == rx_api_response.id() {
                 
-                match data {
+                match rx_api_response.recv_sync() {
                     
-                    Some(api::Response::GetFile(result, callback)) => {
+                    Ok(api::Response::GetFile(result, callback)) => {
                         
                         log::debug(format!("Received file data"));
                         callback.call_box((result,));
                     }
                     
-                    None => {
+                    Err(err) => {
 
                         //TODO
-                        log::info(format!("wyparował nadawca requestów"));
+                        log::info(format!("rx_api_response channel error: {:?}", err));
                         return;
                     }
                 }
