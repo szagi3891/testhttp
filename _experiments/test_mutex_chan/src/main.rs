537bab37392fe44a4f8cc1f6ef3be3be3e9e93fa
szagi3891 <szeligagrzegorz@gmail.com>
2016-01-24 23:13:39 +0100

    mutex example

--- _experiments/test_mutex_chan/src/main.rs
+++ _experiments/test_mutex_chan/src/main.rs
@@ -1,75 +1,30 @@
-use std::sync::{Arc, Mutex, Condvar};
-use std::thread;
-
-struct SuperMutex<T> {
-    mutex : Mutex<Option<T>>,
-    cond  : Condvar,
+fn chan() {
 }
 
-impl<T> SuperMutex<T> {
-    
-    fn new() -> SuperMutex<T> {
-        
-        SuperMutex{
-            mutex : Mutex::new(None),
-            cond  : Condvar::new(),
-        }
-    }
-    
-    fn save(&self, new_value: T) {
-        
-        let mut value = self.mutex.lock().unwrap();
-        *value = Some(new_value);
-        self.cond.notify_one();
-        
-        //notify_all?
-    }
-    
-    fn get(&self) -> T {
-        
-        let mut value_opt = self.mutex.lock().unwrap();
-        
-        loop {
-            
-            let value = value_opt.take();
-            
-            match value {
-
-                Some(value) => {
-                    return value;
-                }
-                
-                None => {
-                    println!("dalej pusta wartość w schowku, czekam dalej");
-                }
-            }
-            
-            value_opt = self.cond.wait(value_opt).unwrap();
-        }
-    }
+struct In {
+    query : Arc<Mutex<StateQuery>>,
 }
+
+struct StateQuery {
+    list : Vec<Sender>,
+}
+
+struct Sender {
+    query : Arc<Mutex<In>>,
+    chan  : Arc<Mutex<StateChan>>,
+}
+
+struct StateChan {
+    name : String
+}
+
+//in
+//stan
+//sender
+//stan kanału
+
 
 fn main() {
     
-    println!("test ...");
-    
-    let mut mu1 = Arc::new(SuperMutex::new());
-    let mut mu2 = mu1.clone();
-    let mut mu3 = mu1.clone();
-    
-    thread::spawn(move|| {
-        mu2.save("value thread1");
-    });
-    
-    thread::spawn(move|| {
-        mu3.save("value thread2");
-    });
-    
-    let value_from_thread = mu1.get();
-    println!("odebrana wartość z wątku {}", value_from_thread);
-    
-    let value_from_thread = mu1.get();
-    println!("odebrana wartość z wątku {}", value_from_thread);
+    println!("test ... zx");
 }
-
-
