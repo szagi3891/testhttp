e362dd1fe08ddb80b9b3dccb4cdec9ffac7e6a48
szagi3891 <szeligagrzegorz@gmail.com>
2016-02-17 07:19:29 +0100

    channels: enum

---
+++ _experiments/test_enum/src/main.rs
@@ -0,0 +1,35 @@
+use std::sync::Arc;
+
+fn main() {
+    println!("Hello, func world!");
+}
+
+pub type StaticFunc<A,B> = Fn(A) -> B + 'static + Send + Sync;
+
+pub trait Convert<A,C> {
+    fn conv(&self, A) -> C;
+}
+
+pub enum Fnconvert<A,B,C> {
+    First(Arc<Box<StaticFunc<A,C>>>),
+    Next (Arc<Box<StaticFunc<A,B>>>, Box<Convert<B,C>>)
+}
+
+impl<A,B,C> Convert<A,C> for Fnconvert<A,B,C> {
+    
+    fn conv(&self, value : A) -> C {
+        
+        match self {
+            
+            &Fnconvert::First(ref func) => {
+                
+                func(value)
+            },
+            
+            &Fnconvert::Next(ref func, ref nextConv) => {
+                
+                nextConv.conv(func(value))
+            }
+        }
+    }
+}
