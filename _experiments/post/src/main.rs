dc01df7d8e115a318af85bb1cd20d60c09c7a402
szagi3891 <szeligagrzegorz@gmail.com>
2016-04-21 22:39:41 +0200

    post experiments

--- _experiments/post/src/main.rs
+++ _experiments/post/src/main.rs
@@ -1,78 +1,129 @@
 use std::collections::HashMap;
 
+pub type HashMapFn<T> = HashMap<String, Box<Fn(&mut T, Vec<u8>)>>;
 
-struct user {
-    
-    login : String,
-    pass  : String,
+pub trait ModelBind {
+    fn model_bind() -> (Self, HashMapFn<Self>);
 }
 
-impl user {
+
+#[derive(Default)]
+struct User {
     
-    fn model_builder() -> (user, HashMap<String, user>) {
+    login : Vec<u8>,
+    pass  : Vec<u8>,
+}
+
+impl ModelBind for User {
+    
+    fn model_bind() -> (User, HashMapFn<User>) {
         
-        let model = user {
-            login : "".to_owned(),
-            pass  : "".to_owned(),
-        };
+        let model = Default::default();
         
-        let map = HashMap::new();
+        let mut map: HashMapFn<User> = HashMap::new();
+        
+        map.insert("login".to_owned(), Box::new(|model: &mut User, value: Vec<u8>|{
+            model.login = From::from(value);
+        }));
+        
+        map.insert("pass".to_owned() , Box::new(|model: &mut User, value: Vec<u8>|{
+            model.pass = From::from(value);
+        }));
         
         (model, map)
     }
 }
 
 
-struct model_builder<T> {
+struct ModelBuilder<T> where T : ModelBind {
     model : T,
-    map   : HashMap<String, T>,
+    map   : HashMapFn<T>,
 }
 
-impl<T> model_builder<T> {
+impl<T> ModelBuilder<T> where T : ModelBind {
     
-    fn new(inject : (T, HashMap<String, T>)) -> model_builder<T> {
+    fn new() -> ModelBuilder<T> {
         
-        let (model, map) = inject;
+        let (model, map) = T::model_bind();
         
-        model_builder {
+        ModelBuilder {
             model : model,
             map   : map,
         }
     }
     
-    fn set(&mut self, value: Vec<u8>) -> bool {
+    fn set(&mut self, field: String, value: Vec<u8>) -> bool {
         
+        //TODO
         false
+    }
+    
+    fn get() -> Option<T> {
+        
+        //TODO
+        None
     }
 }
 
-
-fn main() {
+fn make_model(login: Vec<u8>, pass: Vec<u8>) -> Option<User> {
     
-    let login    : Vec<u8> = "Grzegorz".to_owned().into_bytes();
-    let password : Vec<u8> = "tajne hasło".to_owned().into_bytes();
     
-    let builder = model_builder::new(user::model_builder());
+    let mut builder : ModelBuilder<User> = ModelBuilder::new();
     
-    /*
-    
-    let konwerter = konwerter::new::<user>();
-    
-    let result =  konwerter.set("login", login);
-    
-    if result == false {
+    if builder.set("login".to_owned(), login) == false {
         return None
     }
     
-    let result2 = konwerter.set("password", password);
-    
-    if result2 == false {
+    if builder.set("pass".to_owned(), pass) == false {
         return None;
     }
     
-    konwerter.get()         //zwraca opcję
+    builder.get()
     
-    */
+    None
+}
+
+fn main() {
+    
+    let login : Vec<u8> = "Grzegorz".to_owned().into_bytes();
+    let pass  : Vec<u8> = "tajne hasło".to_owned().into_bytes();
+    
+    let user_opt = make_model(login, pass);
+    
+    
     
     println!("Hello, world!");
 }
+
+
+
+
+/*
+impl From<io::Error> for CliError {
+    fn from(err: io::Error) -> CliError {
+        CliError::Io(err)
+    }
+}*/
+
+/*
+    w przypadku przenoszenia pól, np. do zapytania wstawiającego dane do bazy, zrobić iterator jakiś np.
+*/
+
+
+/*
+http://blog.burntsushi.net/rust-error-handling/
+
+trait From<T> {
+    fn from(T) -> Self;
+}
+
+impl From<io::Error> for CliError {
+    fn from(err: io::Error) -> CliError {
+        CliError::Io(err)
+    }
+}
+
+let string: String = From::from("foo");
+let bytes: Vec<u8> = From::from("foo");
+
+*/
