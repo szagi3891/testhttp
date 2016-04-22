69f15468d478c71eccfa1eaa9cef0db1db5df3db
szagi3891 <szeligagrzegorz@gmail.com>
2016-04-22 05:49:22 +0200

    experiments

--- _experiments/post/src/main.rs
+++ _experiments/post/src/main.rs
@@ -1,39 +1,16 @@
 use std::collections::HashMap;
+use std::str;
 
-pub type HashMapFn<T> = HashMap<String, Box<Fn(&mut T, Vec<u8>)>>;
+pub type HashMapFn<T> = HashMap<String, Box<Fn(&mut T, Vec<u8>) -> bool>>;
 
 pub trait ModelBind {
     fn model_bind() -> (Self, HashMapFn<Self>);
+    //fn bind() -> Option<Self>;
 }
 
-
-#[derive(Default)]
-struct User {
-    
-    login : Vec<u8>,
-    pass  : Vec<u8>,
+pub trait ModelConvert<T> where Self: Sized {
+    fn from(T) -> Result<Self, ()>;
 }
-
-impl ModelBind for User {
-    
-    fn model_bind() -> (User, HashMapFn<User>) {
-        
-        let model = Default::default();
-        
-        let mut map: HashMapFn<User> = HashMap::new();
-        
-        map.insert("login".to_owned(), Box::new(|model: &mut User, value: Vec<u8>|{
-            model.login = From::from(value);
-        }));
-        
-        map.insert("pass".to_owned() , Box::new(|model: &mut User, value: Vec<u8>|{
-            model.pass = From::from(value);
-        }));
-        
-        (model, map)
-    }
-}
-
 
 struct ModelBuilder<T> where T : ModelBind {
     model : T,
@@ -54,19 +31,100 @@ impl<T> ModelBuilder<T> where T : ModelBind {
     
     fn set(&mut self, field: String, value: Vec<u8>) -> bool {
         
-        //TODO
-        false
+        match self.map.remove(&field) {
+            Some(fn_set) => {
+                fn_set(&mut self.model, value);
+                true
+            },
+            None => {
+                false
+            },
+        }
     }
     
-    fn get() -> Option<T> {
+    fn get(self) -> Option<T> {
         
-        //TODO
-        None
+        if self.map.len() == 0 {
+            Some(self.model)
+        } else {
+            None
+        }
     }
 }
 
-fn make_model(login: Vec<u8>, pass: Vec<u8>) -> Option<User> {
+#[derive(Default, Debug)]
+struct PostField {
+    value : Vec<u8>,
+}
+
+
+impl ModelConvert<Vec<u8>> for PostField {
     
+    fn from(value: Vec<u8>) -> Result<PostField, ()> {
+        
+        Ok(PostField {
+            value : value
+        })
+    }
+}
+
+impl ModelConvert<Vec<u8>> for String {
+    
+    fn from(value: Vec<u8>) -> Result<String, ()> {
+        
+        match str::from_utf8(&value) {
+            Ok(v) => {
+                Ok(v.to_owned())
+            },
+            Err(_) => Err(()),
+        }
+    }
+}
+
+
+#[derive(Default, Debug)]
+struct User {
+    
+    login : String,
+    pass  : String,
+    bad   : String,
+}
+
+impl ModelBind for User {
+    
+    fn model_bind() -> (User, HashMapFn<User>) {
+        
+        let model = Default::default();
+        
+        let mut map: HashMapFn<User> = HashMap::new();
+        
+        map.insert("login".to_owned(), Box::new(|model: &mut User, value: Vec<u8>| -> bool {
+            match ModelConvert::from(value) {
+                Ok(value) => {
+                    model.login = value;
+                    true
+                },
+                Err(()) => false,
+            }
+        }));
+        
+        map.insert("pass".to_owned() , Box::new(|model: &mut User, value: Vec<u8>| -> bool {
+            
+            match ModelConvert::from(value) {
+                Ok(value) => {
+                    model.pass = value;
+                    true
+                },
+                Err(()) => false,
+            }
+        }));
+        
+        (model, map)
+    }
+}
+
+
+fn make_model(login: Vec<u8>, pass: Vec<u8>, bad: Vec<u8>) -> Option<User> {
     
     let mut builder : ModelBuilder<User> = ModelBuilder::new();
     
@@ -78,21 +136,33 @@ fn make_model(login: Vec<u8>, pass: Vec<u8>) -> Option<User> {
         return None;
     }
     
-    builder.get()
+    if builder.set("bad".to_owned(), bad) == false {
+        return None;
+    }
     
-    None
+    builder.get()
 }
 
 fn main() {
     
     let login : Vec<u8> = "Grzegorz".to_owned().into_bytes();
     let pass  : Vec<u8> = "tajne hasło".to_owned().into_bytes();
+    let bad   : Vec<u8> = vec![129, 129, 129];
     
-    let user_opt = make_model(login, pass);
+    let user_opt = make_model(login, pass, bad);
     
+    /*
+    match user_opt {
+        Some(user) => {
+            
+            match login.String() && pass.String() {
+                
+            }
+        }
+    }
+    */
     
-    
-    println!("Hello, world!");
+    println!("Hello, world! {:?}", user_opt);
 }
 
 
@@ -103,7 +173,8 @@ impl From<io::Error> for CliError {
     fn from(err: io::Error) -> CliError {
         CliError::Io(err)
     }
-}*/
+}
+*/
 
 /*
     w przypadku przenoszenia pól, np. do zapytania wstawiającego dane do bazy, zrobić iterator jakiś np.
