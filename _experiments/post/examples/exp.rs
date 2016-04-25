7e85cb92dfd0b3cd656844b19b1b5d2f9884af2d
szagi3891 <szeligagrzegorz@gmail.com>
2016-04-25 05:26:07 +0200

    macro for create serializable struct

--- _experiments/post/examples/exp.rs
+++ _experiments/post/examples/exp.rs
@@ -1,60 +1,18 @@
-macro_rules! log {
-    ($msg:expr) => {{
-        let state: i32 = get_log_state();
-        if state > 0 {
-            println!("log({}): {}", state, $msg);
-        }
-    }};
-}
-
-//ident
-
-macro_rules! serialize {
-    
-    ($msg:item) => {{
-        
-        $msg
-        
-        
-    }};
-}
-
-serialize!(struct User {
-    
-    login : String,
-    pass  : String,
-    bad   : String,
-});
-
-
-fn main() {
-    let state: &str = "reticulating splines";
-    log!(state);
-}
-
-/*
-macro_rules! e_class(
-    ($S_name:ident,
-     $( $method_name:ident $enum_name:ident ),*)
-    =>
-    {
-        pub impl $S_name {
-            pub fn name(index:uint) -> &'static str {
-                match index {
-                    0 => "nil", 1 => "lft", 2 => "rgt", 3 => "all",
-                    4 => "end", _ => "???"
-                }
-            }
-
-            $( pub fn $method_name (&self) -> int { self.items[$enum_name as uint] } )*
-        }
+macro_rules! struct_serialize {
+    ($name:ident => $($element: ident: $ty: ty),*) => {
+        struct $name { $($element: $ty),* }
     }
-)
+}
 
-e_class!( S,
-          nil_item Enil ,
-          rgt_item Ergt ,
-          lft_item Elft ,
-          all_item Eall ,
-          end_item Eend )
-*/
+struct_serialize!(
+    User =>
+        x : u8,
+        y : String,
+        z : u32
+);
+
+
+fn main() { 
+    
+    println!("hello");
+}
